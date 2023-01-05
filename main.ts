cb7c48bd058b0d9d4270d9a165188dc206560772
418181 <115142558+418181@users.noreply.github.com>
2023-01-05 16:41:14 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    game.gameOver()
+})
 let 主角 = game.createSprite(2, 2)
 let 目標 = game.createSprite(randint(0, 4), randint(0, 4))
 game.setScore(0)
 basic.forever(function () {
     if (input.isGesture(Gesture.TiltLeft)) {
         主角.change(LedSpriteProperty.X, 1)
-        basic.pause(100)
+        basic.pause(200)
     } else if (input.isGesture(Gesture.TiltRight)) {
         主角.change(LedSpriteProperty.X, -1)
-        basic.pause(100)
+        basic.pause(200)
     } else if (input.isGesture(Gesture.LogoUp)) {
         主角.change(LedSpriteProperty.Y, -1)
-        basic.pause(100)
+        basic.pause(200)
     } else if (input.isGesture(Gesture.LogoDown)) {
         主角.change(LedSpriteProperty.Y, 1)
-        basic.pause(100)
+        basic.pause(200)
     }
 })
 basic.forever(function () {
